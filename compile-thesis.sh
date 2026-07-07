2da8d4d7f44716c8d75538bb94ebd15bf735d47d
hyperionx <mr_vcv@hotmail.co.uk>
2026-07-07 11:57:15 +0100

    Update dissertation without appendices

--- compile-thesis.sh
+++ compile-thesis.sh
@@ -1,87 +1,68 @@
 #!/bin/bash
-# A script to compile the PhD Thesis - Krishna Kumar 
+# A script to compile the PhD Thesis
+# Uses latexmk to run only the passes actually needed.
 # Distributed under GPLv2.0 License
 
-compile="compile";
-clean="clean";
+set -e
 
-if test -z "$2"
-then
-if [ $1 = $clean ]; then
-	echo "Cleaning please wait ..."
-	rm -f *~
-	rm -rf *.aux
-	rm -rf *.bbl
-	rm -rf *.blg
-	rm -rf *.d
-	rm -rf *.fls
-	rm -rf *.ilg
-	rm -rf *.ind
-	rm -rf *.toc*
-	rm -rf *.lot*
-	rm -rf *.lof*
-	rm -rf *.log
-	rm -rf *.idx
-	rm -rf *.out*
-	rm -rf *.nlo
-	rm -rf *.nls
-	rm -rf $filename.pdf
-	rm -rf $filename.ps
-	rm -rf $filename.dvi
-	rm -rf *#* 
-	echo "Cleaning complete!"
-	exit
-else
+usage() {
 	echo "Shell script for compiling the PhD Thesis"
-	echo "Usage: sh ./compile-thesis.sh [OPTIONS] [filename]"
-	echo "[option]  compile: Compiles the PhD Thesis"
-	echo "[option]  clean: removes temporary files no filename required"
-	exit
-fi
-fi
+	echo "Usage: ./compile-thesis.sh [OPTIONS] [filename]"
+	echo "  compile [filename]   Compile via latexmk (default filename: thesis)"
+	echo "  draft   [filename]   Fast draft pass (no figure embedding)"
+	echo "  watch   [filename]   Continuous build on file change"
+	echo "  figs                 Convert all .eps figures to .pdf"
+	echo "  clean   [filename]   Remove auxiliary files"
+	echo "  cleanall [filename]  Remove auxiliary files and the output PDF"
+	exit 1
+}
 
-filename=$2;
+[ -z "$1" ] && usage
 
-if [ $1 = $clean ]; then
-	echo "Cleaning please wait ..."
-	rm -f *~
-	rm -rf *.aux
-	rm -rf *.bbl
-	rm -rf *.blg
-	rm -rf *.d
-	rm -rf *.fls
-	rm -rf *.ilg
-	rm -rf *.ind
-	rm -rf *.toc*
-	rm -rf *.lot*
-	rm -rf *.lof*
-	rm -rf *.log
-	rm -rf *.idx
-	rm -rf *.out*
-	rm -rf *.nlo
-	rm -rf *.nls
-	rm -rf $filename.pdf
-	rm -rf $filename.ps
-	rm -rf $filename.dvi
-	rm -rf *#* 
-	echo "Cleaning complete!"
-	exit
-elif [ $1 = $compile ]; then
-	echo "Compiling your PhD Thesis...please wait...!"
-	pdflatex -interaction=nonstopmode $filename.tex
-	bibtex $filename.aux 	
-	makeindex $filename.aux
-	makeindex $filename.idx
-	makeindex $filename.nlo -s nomencl.ist -o $filename.nls
-	pdflatex -interaction=nonstopmode $filename.tex
-	makeindex $filename.nlo -s nomencl.ist -o $filename.nls
-	pdflatex -interaction=nonstopmode $filename.tex
-	echo "Success!"
-	exit
-fi
+cmd="$1"
+filename="${2:-thesis}"
 
-
-if test -z "$3"
-then
-	exit
-fi
+case "$cmd" in
+	figs)
+		echo "Converting EPS figures to PDF..."
+		find . -name '*.eps' -print0 | while IFS= read -r -d '' f; do
+			pdf="${f%.eps}.pdf"
+			if [ ! -f "$pdf" ] || [ "$f" -nt "$pdf" ]; then
+				echo "  $f"
+				epstopdf "$f"
+			fi
+		done
+		echo "Done."
+		;;
+	compile)
+		echo "Compiling $filename.tex with latexmk..."
+		if ! latexmk -pdf -f "$filename.tex"; then
+			echo "First pass failed. Removing stale aux/out files and retrying..."
+			rm -f "$filename.out" "$filename.aux" "$filename.toc"
+			latexmk -pdf -f "$filename.tex"
+		fi
+		echo "Success! -> $filename.pdf"
+		;;
+	draft)
+		echo "Draft compile (no figure embedding)..."
+		latexmk -pdf -e '$pdflatex=q/pdflatex -draftmode -interaction=nonstopmode %O %S/' "$filename.tex"
+		latexmk -pdf "$filename.tex"
+		;;
+	watch)
+		echo "Watching $filename.tex for changes (Ctrl-C to stop)..."
+		latexmk -pdf -pvc "$filename.tex"
+		;;
+	clean)
+		echo "Cleaning auxiliary files..."
+		latexmk -c "$filename.tex"
+		echo "Cleaning complete!"
+		;;
+	cleanall)
+		echo "Cleaning all generated files..."
+		latexmk -C "$filename.tex"
+		echo "Cleaning complete!"
+		;;
+	*)
+		usage
+		;;
+esac
